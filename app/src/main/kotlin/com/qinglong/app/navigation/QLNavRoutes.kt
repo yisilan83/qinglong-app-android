8c75e348b944959022949ae771437893e26fd78c
yisilan83 <141352987+yisilan83@users.noreply.github.com>
2026-05-07 09:09:09 +0800

    cleanup: remove old TasksRoute (now in feature:task module)

--- app/src/main/kotlin/com/qinglong/app/navigation/QLNavRoutes.kt
+++ app/src/main/kotlin/com/qinglong/app/navigation/QLNavRoutes.kt
@@ -4,7 +4,6 @@ import kotlinx.serialization.Serializable
 
 @Serializable object LoginRoute
 @Serializable object HomeRoute
-@Serializable object TasksRoute
 @Serializable object ScriptsRoute
 @Serializable object EnvRoute
 @Serializable object SettingsRoute
